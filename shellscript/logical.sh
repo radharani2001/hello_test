98b5569b5bd7be6bdbab850465898fcfb2e50ae1
radharani2001 <siritika54@gmail.com>
2025-01-02 20:49:43 +0530

    commited

---
+++ shellscript/logical.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#logical operator
+
+
+
+#we use to write multiple condition validate conditions and execute the statement
+
+And (-a) (&&) OR (-o) || NOT (-n) !
+
+
+T -T - T      T -F - t    T f 
+T -F - F      F- T - t    F t 
+F -T -F      F - F- F      
+F -F -F      T -T -T   
+
+
+ 
