7abd9fac34d821c872f7aeccc30a863a11715279
radharani2001 <siritika54@gmail.com>
2025-01-02 20:54:29 +0530

    uploaded

---
+++ shellscript/arthmethic1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#taking input from users
+echo -e "Enter value: \c"
+read -r a
+echo -e "Enter value: \c"
+read -r b
+
+
+
+echo "addition values `expr $a + $b ` " 
+
+
+
+
+echo "minus values `expr $a - $b ` " 
+
+echo "multiplication values `expr $a \* $b`"
+echo "divided by values `expr $a / $b` "
+
+echo "remainder values `expr $a % $b` "
+echo "Addition of two number is ` expr $a + $b ` " 
+
+
+
+
+
+
+
+
+
+
---
+++ shellscript/countargs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#purpose: Counting given postional parameters
+echo "Your current given parameters are equal to $#"
+#this one will calculate how many parameters you gave as a input to your script
+if [ $#  -1t ];then
+echo "Program Usage is './scriptname.sh' options "
+else "Program executed successfully"
+fi
+
+ 
+
+
+ 
+
+
+
+
+
+
