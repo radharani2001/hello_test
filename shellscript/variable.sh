5da8831daf76d333dae24792b29fb22e531d5329
radharani2001 <siritika54@gmail.com>
2025-01-02 20:38:08 +0530

    variable

---
+++ shellscript/variable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#purpose: what is variable how is help us in writing shell scripts
+A=10
+B=23
+Hostname=$(hostname)
+DATE=`date`
+1value=333
+False@Var=False
+Hyphen-a=WrongValue
+echo "Variable A Value : $A"
+echo "Variable B value: $B" 
+echo "Variable DATE value : $DATE"
+echo "Wrong Variable 1value $1value"
+echo 'False @ Variable' $False@Var
+
+
+
+
+
+
